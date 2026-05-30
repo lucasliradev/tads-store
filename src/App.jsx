779f656598a5f6f03976db8e8bd1cbdd84c84b81
Lucas Lira <lucaslscunha@gmail.com>
2026-05-30 10:32:59 -0300

    Atualizando main.jsx e app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Layout from "./components/Layout";
+import Vitrine from "./components/Vitrine";
+
+export default function App() {
+  return (
+    <Layout>
+      <Vitrine />
+    </Layout>
+  );
+}
