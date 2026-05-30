d67c2610a5e547e395bf1ac7e453095304c87684
Lucas Lira <lucaslscunha@gmail.com>
2026-05-30 13:24:52 -0300

    adicionada logo do ifes e app.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Layout from "./components/Layout";
 import Vitrine from "./components/Vitrine";
+import "./App.css";
 
 export default function App() {
   return (
--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -2,6 +2,7 @@ export default function Cabecalho({ titulo = "TADS Store" }) {
   return (
     <header className="cabecalho">
       <div className="cabecalho_titulo">
+        <img src="/images/ifes_logo.png" alt="Logo IFES" className="cabecalho_img" />
           {titulo}
         </div>
     </header>
